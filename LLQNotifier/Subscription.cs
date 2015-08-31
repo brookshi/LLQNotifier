9b06a67de7233e12bd476848017f02d7c9bce42e
brookshi <iwxiaot@gmail.com>
2015-08-31 10:30:29 +0800

    adjust code

--- LLQNotifier/Subscription.cs
+++ LLQNotifier/Subscription.cs
@@ -15,11 +15,6 @@
 #endregion
 
 using System;
-using System.Reflection;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LLQ
 {
